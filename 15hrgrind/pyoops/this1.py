8ec80868b3991d3571c2fd9afe0245fdaf3d2f8e
swapnilAngarkhe <swapnilangarkhe21@gmail.com>
2023-09-08 02:09:20 +0530

    py oops code w harry all done

--- 15hrgrind/pyoops/this1.py
+++ 15hrgrind/pyoops/this1.py
@@ -24,31 +24,17 @@ class Employee:
             return False
         else:
             return True
-class Programer(Employee):
-    def __init__(self, fname, lname, salary, proglang, exp):
-        super().__init__(fname, lname, salary)
-        self.proglang=proglang
-        self.exp=exp
-
-
-
-harry=Programer("harry","k", 50000, "python", "3 years")
-
         
-Harry=Employee("Harry", "k", 43000)
-rohan=Employee("rohan", "k", 74000)
-lovish=Employee("lovish","L", 67000)
-shumbham=Employee("shumbham","c", 88000)
-
-print(harry.exp, harry.proglang)
-
-# print(Employee.isopen("sunday"))
-
-# print(lovish.salary)
+    def __add__(self,other):
+        return self.salary + other.salary
+    
+    def __repr__(self):
+        return 'Employee({}, {}, {})'.format (self.fname,self.lname, self.salary)
+    
 
 
+harry=Employee("harry","k", 50000)
+rohan=Employee("rohan","das", 9)
 
-#print(Harry.salary)
-#Employee.change_increment(3)
-#Harry.increase()
-#print(Harry.salary)
+
+print(repr (harry))
