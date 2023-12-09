b2d6bd64ba6df7394153b0ba564b60e1b731f9dd
swapnilAngarkhe <swapnilangarkhe21@gmail.com>
2023-12-09 23:27:01 +0530

    mid week9

--- python_cs50/week8/hat.py
+++ python_cs50/week8/hat.py
@@ -3,10 +3,10 @@ import random
 class Hat:
     houses=["Griffindoor", "Hufflepuff", "Ravenclaw", "Slythrin" ]
         
-    def sort(cls,name):
+    @classmethod
+    def sort(cls,name): # here we can use "cls" to access the stuff in the class.
         
         print(f"{name} is in", random.choice(cls.houses))
         
     
-hat = Hat()
-hat.sort("Harry")
+Hat.sort("Harry") 
