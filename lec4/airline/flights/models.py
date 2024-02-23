d9e5c1c3acec43c800509e909ff86af5b9171b1a
swapnilAngarkhe <swapnilangarkhe21@gmail.com>
2024-02-23 16:32:39 +0530

    lec4 complete

--- lec4/airline/flights/models.py
+++ lec4/airline/flights/models.py
@@ -13,7 +13,7 @@ class Airport(models.Model):
 class Flight (models.Model):
     origin=models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='departuers')
     destination=models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='arrivals')
-    duriation=models.IntegerField()
+    duration=models.IntegerField()
     
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
--- lec4/airline/users/urls.py
+++ lec4/airline/users/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", views.index, name='index'),
-    path("<int:flight_id>", views.flight, name='flight'),
-    path("<int:flight_id>/book", views.book, name='book'),
+    path("login", views.login_view, name='login'),
+    path("logout", views.logout_view, name='logout'),
+    
 ] 
